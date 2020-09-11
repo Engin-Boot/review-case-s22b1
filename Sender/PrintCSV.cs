85f9cef2c1c74c4d0abdbf05578d3d5a7967d3e5
Varnika Gautam <54426822+varnikagautam@users.noreply.github.com>
2020-09-11 19:15:36 +0530

    Add files via upload

---
+++ Sender/PrintCSV.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class PrintCSV
+    {
+        /// <summary>
+        /// Print all the rows of CSV File 
+        /// </summary>
+        /// <param name="lines"> </param>
+       
+        public static void PrintCSVRows(string[] lines)
+        {
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                PrintCSVCol(columns);
+                Console.Write("\n");
+            }
+        }
+
+        /// <summary>
+        /// Prints the column of CSV File
+        /// </summary>
+        /// <param name="columns"></param>
+        private static void PrintCSVCol(string[] columns)
+        {
+            foreach (string column in columns)
+            {
+                Console.Write("\t{0}", column);
+            }
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Sender
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string CSVPath = @"C:\VARNIKA\CommentFileCSV.csv";
+            ReadCSV(CSVPath);
+            Console.ReadKey();
+        }
+
+        /// <summary> 
+        /// This function reads the CSV File
+        /// </summary>
+        /// <param name="CSVPath"> CSV File Path </param>
+
+        public static void ReadCSV(string CSVPath)
+        {
+            string[] lines = File.ReadAllLines(CSVPath);
+            Console.WriteLine("\n\tCSV Data \n");
+            PrintCSV.PrintCSVRows(lines);
+        }          
+    }
+}
+
