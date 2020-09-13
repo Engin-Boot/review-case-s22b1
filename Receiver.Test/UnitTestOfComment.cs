4f75781b99767b5d3ed54f5258a1175fec5b53e5
VinayBhutange <kvinay573@gmail.com>
2020-09-13 15:16:40 +0530

    added testcase

--- Receiver.Test/UnitTestOfComment.cs
+++ Receiver.Test/UnitTestOfComment.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Receiver.Test
@@ -11,6 +8,17 @@ namespace Receiver.Test
     public class UnitTestOfComment
     {
         [TestMethod]
-        public void 
+        public void CheckForRightDictionary()
+        {
+            CommentSplitter commentSplitter = new CommentSplitter();
+            SortedDictionary<string, int> wordCount = new SortedDictionary<string, int>();
+            string comment = "void function remove function void";
+            SortedDictionary<string, int> keyValuePairsExpected = new SortedDictionary<string, int>();
+            keyValuePairsExpected.Add("function", 2);
+            keyValuePairsExpected.Add("remove", 1);
+            keyValuePairsExpected.Add("void", 2);
+            wordCount = commentSplitter.SplitCommentsIntoWords(wordCount,comment);
+            Assert.IsTrue(keyValuePairsExpected.SequenceEqual(wordCount));
+        }
     }
 }
