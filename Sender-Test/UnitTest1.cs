6b461488b79ecd51dfacbd9c199478de44d6e2e1
varnikagautam <varnikagautam@users.noreply.github.com>
2020-09-15 14:25:43 +0530

    update sender test

--- Sender-Test/UnitTest1.cs
+++ Sender-Test/UnitTest1.cs
@@ -1,23 +1,25 @@
 using Xunit;
 using System.IO;
 using System;
+using Sender;
 
 namespace Sender_Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void FileExistTest()
         {
+            String path = @"C:\\VARNIKA\\Test.csv";
             try
             {
                 if (File.Exists("C:\\VARNIKA\\Test.csv"))
-                    return; // indicates success
+                    Program.ReadCSV(path); // indicates success
             }
             catch (Exception ex)
             {
                 Assert.True(false, ex.Message);
             }
-        }
+        }                
     }  
 }
