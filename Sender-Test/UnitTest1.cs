2ed46ae81da79f376d6b5cb6471dbba037a46e37
Varnika Gautam <54426822+varnikagautam@users.noreply.github.com>
2020-09-16 00:10:54 +0530

    Update UnitTest1.cs

--- Sender-Test/UnitTest1.cs
+++ Sender-Test/UnitTest1.cs
@@ -26,7 +26,7 @@ namespace Sender_Test
         [Fact]
         public void ReadFileTest()
         {
-            String path = @"C:\VARNIKA\AssignmentBootcamp\Sender-Test\bin\Debug\netcoreapp3.1\TestFile\TestFile.csv";
+            String path = @" D:\a\review-case-s22b1\review-case-s22b1\Sender-Test\bin\Release\netcoreapp3.1\TestFile\TestFile.csv";
             String[] TestResult = File.ReadAllLines(path);
             String[] ReadCSVResult = Program.ReadCSV(path);
             bool areEqual = TestResult.SequenceEqual(ReadCSVResult);
